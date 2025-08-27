7a7b34967bd9ab81365fc9fdbaa012345963c4d4
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-08-27 16:18:47 +0530

    change key name

--- run-instance.py
+++ run-instance.py
@@ -3,7 +3,7 @@ client = boto3.client('ec2')
 response = client.run_instances(
     ImageId='ami-00ca32bbc84273381',
     InstanceType='t2.micro',
-    KeyName='vaultaccess',
+    KeyName='windows',
     MaxCount=3,
     MinCount=1,
 )
