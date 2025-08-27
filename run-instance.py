a89f532efc417290d0db4b8b7c27cb58c3d1cef1
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-08-27 16:27:31 +0530

    key is winOS

--- run-instance.py
+++ run-instance.py
@@ -4,7 +4,7 @@ client = boto3.client('ec2')
 response = client.run_instances(
     ImageId='ami-00ca32bbc84273381',
     InstanceType='t2.micro',
-    KeyName='windows',
+    KeyName='Windows OS',
     MaxCount=3,
     MinCount=1,
 )
